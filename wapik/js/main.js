6d87c81655007c4f618dcad889acc1aa1b283fa1
Nxisgod007 <jithman.saini@coalitiontechnologies.com>
2022-02-07 15:28:59 -0800

    nav bar fix

--- wapik/js/main.js
+++ wapik/js/main.js
@@ -3,6 +3,9 @@ const navSlide = () =>{
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".header-right-con nav ul");
     const navLinks = document.querySelectorAll(".header-right-con nav ul li");
+    const lineOne = document.querySelector(".class1");
+    const lineTwo = document.querySelector(".class2");
+    const lineThree = document.querySelector(".class3")
     // toggle nav 
 
     burger.addEventListener('click', ()=>{
@@ -16,7 +19,6 @@ const navSlide = () =>{
                 link.style.animation = `navLinkFade 0.5s ease forwards ${i /7 + 0.5}s`;
 
             }
-            
         });
     })
 }
