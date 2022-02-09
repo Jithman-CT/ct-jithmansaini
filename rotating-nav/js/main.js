ee1cd4fc691fce1386b943a049e07952a861a4ab
Nxisgod007 <jithman.saini@coalitiontechnologies.com>
2022-02-09 10:20:59 -0800

    rotating navigation

---
+++ rotating-nav/js/main.js
@@ -0,0 +1,14 @@
+// dom selection
+const open = document.querySelector("#open");
+const close = document.querySelector("#close");
+const container = document.querySelector(".my-container");
+
+
+
+open.addEventListener('click', function () {
+    container.classList.add('show-nav')
+})
+
+close.addEventListener('click', function () {
+    container.classList.remove('show-nav')
+})
